d989b4255030d1230236904aa0b2f48a39ef6955
Maximilian Irro <max@irro.at>
2019-05-02 19:24:26 +0200

    Adds unit test for DateParser

---
+++ src/test/kotlin/io/hemin/wien/parser/DateParserTest.kt
@@ -0,0 +1,23 @@
+package io.hemin.wien.parser
+
+import java.time.Instant
+import java.util.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+// TODO test: 2019-05-01T21:04:13.958+02:00[Europe/Vienna]
+
+class DateParserTest {
+
+    // millis calculated from: "Fri, 16 Mar 2018 22:49:08 +0000"
+    val expected: Date = Date.from(Instant.ofEpochMilli(1521240548000))
+
+    @Test
+    fun testParseDateFormats() {
+
+        assertEquals(expected, DateParser.parse("Fri, 16 Mar 2018 22:49:08 +0000"))
+
+        // TODO add more date formats
+    }
+
+}
