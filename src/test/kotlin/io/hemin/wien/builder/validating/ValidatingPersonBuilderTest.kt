ca94b1eeb50e0d4503fcb5c083ab6e21fb93f4a5
Sebastiano Poggi <poggos@gmail.com>
2021-01-03 16:04:55 +0100

    Write tests for common Validating*Builders

---
+++ src/test/kotlin/io/hemin/wien/builder/validating/ValidatingPersonBuilderTest.kt
@@ -0,0 +1,47 @@
+package io.hemin.wien.builder.validating
+
+import assertk.all
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNotNull
+import assertk.assertions.isNull
+import assertk.assertions.prop
+import io.hemin.wien.model.Person
+import org.junit.jupiter.api.Test
+
+internal class ValidatingPersonBuilderTest {
+
+    @Test
+    internal fun `should not build a person when the mandatory fields are missing`() {
+        val personBuilder = ValidatingPersonBuilder()
+
+        assertThat(personBuilder.build()).isNull()
+    }
+
+    @Test
+    internal fun `should build a person with all the mandatory fields`() {
+        val personBuilder = ValidatingPersonBuilder()
+            .name("name")
+
+        assertThat(personBuilder.build()).isNotNull().all {
+            prop(Person::name).isEqualTo("name")
+            prop(Person::email).isNull()
+            prop(Person::uri).isNull()
+        }
+    }
+
+    @Test
+    internal fun `should build a person with all the optional fields`() {
+        val personBuilder = ValidatingPersonBuilder()
+            .name("name")
+            .email("email")
+            .uri("uri")
+
+        assertThat(personBuilder.build()).isNotNull().all {
+            prop(Person::name).isEqualTo("name")
+            prop(Person::email).isEqualTo("email")
+            prop(Person::uri).isEqualTo("uri")
+        }
+    }
+
+}
