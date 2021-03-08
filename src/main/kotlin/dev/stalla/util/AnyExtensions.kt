cd2c9f6e4eae0b09481fb5616234f4f37ab778fb
Maximilian Irro <max@irro.at>
2021-03-08 12:29:30 +0100

    Simplify allNull and allNotNull

--- src/main/kotlin/dev/stalla/util/AnyExtensions.kt
+++ src/main/kotlin/dev/stalla/util/AnyExtensions.kt
@@ -4,39 +4,17 @@ import kotlin.contracts.contract
 
 /** Check if all argument elements are not null. */
 @InternalApi
-internal fun allNotNull(a: Any?): Boolean {
+internal fun allNotNull(
+    a: Any?,
+    b: Any? = Unit,
+    c: Any? = Unit,
+    d: Any? = Unit
+): Boolean {
     contract {
-        returns(true) implies (a != null)
-        returns(false) implies (a == null)
+        returns(true) implies (a != null && b != null && c != null && d != null)
     }
-    return a != null
+    return a != null && b != null && c != null && d != null
 }
-
-/** Check if all argument elements are not null. */
-@InternalApi
-internal fun allNotNull(a: Any?, b: Any?): Boolean {
-    contract {
-        returns(true) implies (a != null && b != null)
-    }
-    return a != null && b != null
-}
-
-/** Check if all argument elements are not null. */
-@InternalApi
-internal fun allNotNull(a: Any?, b: Any?, c: Any?): Boolean {
-    contract {
-        returns(true) implies (
-            a != null &&
-                b != null &&
-                c != null
-            )
-    }
-    return a != null && b != null && c != null
-}
-
-/** Check if all argument elements are not null. */
-@InternalApi
-internal fun allNotNull(vararg elements: Any?): Boolean = elements.all { p -> p != null }
 
 /** Check if at least one argument element is not null. */
 @InternalApi
@@ -44,39 +22,17 @@ internal fun anyNotNull(vararg elements: Any?): Boolean = elements.any { p -> p 
 
 /** Check if all argument elements are null. */
 @InternalApi
-internal fun allNull(a: Any?): Boolean {
+internal fun allNull(
+    a: Any?,
+    b: Any? = Unit,
+    c: Any? = Unit,
+    d: Any? = Unit
+): Boolean {
     contract {
-        returns(true) implies (a == null)
-        returns(false) implies (a != null)
+        returns(true) implies (a == null && b == null && c == null && d == null)
     }
-    return a == null
+    return a == null && b == null && c == null && d == null
 }
-
-/** Check if all argument elements are null. */
-@InternalApi
-internal fun allNull(a: Any?, b: Any?): Boolean {
-    contract {
-        returns(true) implies (a == null && b == null)
-    }
-    return a == null && b == null
-}
-
-/** Check if all argument elements are null. */
-@InternalApi
-internal fun allNull(a: Any?, b: Any?, c: Any?): Boolean {
-    contract {
-        returns(true) implies (
-            a == null &&
-                b == null &&
-                c == null
-            )
-    }
-    return a == null && b == null && c == null
-}
-
-/** Check if all argument elements are null. */
-@InternalApi
-internal fun allNull(vararg elements: Any?): Boolean = elements.all { p -> p == null }
 
 /** Check if at least one argument element is null. */
 @InternalApi
