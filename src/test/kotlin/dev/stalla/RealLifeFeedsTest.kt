771816fbc1a94a5b5944357d686174f652c86362
Maximilian Irro <max@irro.at>
2021-03-19 17:25:21 +0100

    Use @TemporaryFile for real life feed tests

--- src/test/kotlin/dev/stalla/RealLifeFeedsTest.kt
+++ src/test/kotlin/dev/stalla/RealLifeFeedsTest.kt
@@ -5,40 +5,35 @@ import assertk.assertThat
 import assertk.assertions.exists
 import assertk.assertions.isEqualTo
 import assertk.assertions.isNotNull
-import org.junit.jupiter.api.DynamicTest
-import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
 import java.io.File
 
+@ExtendWith(TemporaryFileParameterResolver::class)
 internal class RealLifeFeedsTest {
 
-    private val resourceFilesPath = "/xml/real-life-feeds"
+    private class RealLifeFeedsProvider : ArgumentsProvider by arguments(*allResourceFilesIn(resourceFilesPath).toTypedArray())
 
-    @TestFactory
-    fun dynamicTestsWithCollection(): Collection<DynamicTest> {
-        val testFiles = allResourceFilesIn(resourceFilesPath)
-
-        return testFiles.map { testFile ->
-            DynamicTest.dynamicTest(
-                "[${testFile.nameWithoutExtension}] should be able to parse, write and re-parse a real-life feed correctly"
-            ) { testRealLifeFeed(testFile) }
-        }
-    }
-
-    private fun testRealLifeFeed(file: File) {
+    @ParameterizedTest
+    @ArgumentsSource(RealLifeFeedsProvider::class)
+    fun `should be able to parse, write and re-parse a real-life feed correctly`(testFile: File, @TemporaryFile tmpFile: File) {
         assertAll {
-            val parsedPodcast = PodcastRssParser.parse(file)
+            val parsedPodcast = PodcastRssParser.parse(testFile)
             assertThat(parsedPodcast, "parsed podcast").isNotNull()
 
-            val tmpFile = File.createTempFile("stalla_test", "writer_output")
             PodcastRssWriter.write(parsedPodcast!!, tmpFile)
 
             assertThat(tmpFile, "written file").exists()
             assertThat(tmpFile, "written file").isNotEmpty()
 
             val reparsedPodcast = PodcastRssParser.parse(tmpFile)
-            assertThat(reparsedPodcast, "written file for ${file.nameWithoutExtension} matches original Podcast").isEqualTo(parsedPodcast)
-
-            tmpFile.delete()
+            assertThat(reparsedPodcast, "written file for ${testFile.nameWithoutExtension} matches original Podcast").isEqualTo(parsedPodcast)
         }
+    }
+
+    companion object {
+        const val resourceFilesPath = "/xml/real-life-feeds"
     }
 }
