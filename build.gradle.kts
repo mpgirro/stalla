7b99a3ca8d8e16991c3cdda50c34d919c0ccce2c
Sebastiano Poggi <poggos@gmail.com>
2021-03-23 18:53:01 +0100

    Set up publishing in Gradle buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,14 +34,16 @@ plugins {
     id("org.jetbrains.dokka") version "1.4.30"
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.4.0"
     id("jacoco")
-    id("java")
+    `maven-publish`
+    signing
     id("com.github.nbaztec.coveralls-jacoco") version "1.2.5"
     id("org.jmailen.kotlinter") version "3.3.0"
     id("io.gitlab.arturbosch.detekt") version "1.16.0"
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
 }
 
 group = "dev.stalla"
-version = "0.10.0"
+version = "1.0.0"
 
 val junit5Version = "5.7.0"
 val kotlinVersion = plugins.getPlugin(KotlinPluginWrapper::class.java).kotlinPluginVersion
@@ -79,6 +81,84 @@ detekt {
 
 apiValidation {
     nonPublicMarkers.add("dev.stalla.util.InternalAPI")
+}
+
+val javadocJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaJavadoc.get().outputs)
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(kotlin.sourceSets.main.get().kotlin.srcDirs)
+}
+
+fun searchPropertyOrNull(name: String, vararg aliases: String): String? {
+    fun searchEverywhere(name: String): String? =
+        findProperty(name) as? String? ?: System.getenv(name)
+
+    searchEverywhere(name)?.let { return it }
+
+    aliases.forEach { alias ->
+        searchEverywhere(alias)?.let { return it }
+    }
+
+    return null
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(searchPropertyOrNull("SONATYPE_USERNAME", "sonatypeUsername"))
+            password.set(searchPropertyOrNull("SONATYPE_PASSWORD", "sonatypePassword"))
+        }
+    }
+}
+
+signing {
+    val secretKey: String? = rootProject.file("secring.txt")
+        .takeIf { it.exists() }
+        ?.readText()
+    val password: String? = searchPropertyOrNull("SIGNING_PASSWORD", "signingPassword")
+    useInMemoryPgpKeys(secretKey, password)
+}
+
+publishing {
+    publications.create<MavenPublication>(project.name) {
+        artifact(javadocJar)
+        artifact(sourcesJar)
+        from(components["kotlin"])
+
+        pom {
+            name.set(project.name)
+            description.set("RSS reading and writing library for podcasts, written in Kotlin and Java-friendly")
+            url.set("https://github.com/mpgirro/stalla")
+            scm {
+                url.set("https://github.com/mpgirro/stalla.git")
+            }
+            licenses {
+                license {
+                    name.set("BSD 3-Clause License")
+                    url.set("https://github.com/mpgirro/stalla/blob/master/LICENSE")
+                }
+            }
+            developers {
+                developer {
+                    id.set("mpgirro")
+                    name.set("Maximilian Irro")
+                    url.set("https://github.com/mpgirro")
+                }
+                developer {
+                    id.set("rock3r")
+                    name.set("Sebastiano Poggi")
+                    url.set("https://github.com/rock3r")
+                }
+            }
+        }
+        signing.sign(this)
+    }
 }
 
 tasks {
